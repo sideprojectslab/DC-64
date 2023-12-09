afe4d06100ef10e7fb8da06f2ba5b61f916116df
sideprojectslab <sideprojectslab@gmail.com>
2023-12-09 12:44:34 +0100

    fixed firmware for board revision 0.2

--- dc-64d/firmware/dc64d/main.c
+++ dc-64d/firmware/dc64d/main.c
@@ -16,12 +16,12 @@
 
 #include <xc.h>
 
-#define CORE_CLK_FREQ     8000000
-#define SYS_CLK_PRESCALER 8
+#define CORE_CLK_FREQ     6000000
+#define SYS_CLK_PRESCALER 4
 #define SYS_CLOCK_FREQ    (CORE_CLK_FREQ / SYS_CLK_PRESCALER)
 #define PWM_PRESCALER     1
-#define OCR_60Hz          (uint16_t)(SYS_CLOCK_FREQ / PWM_PRESCALER / 60 / 1.03f / 2)
-#define OCR_50Hz          (uint16_t)(SYS_CLOCK_FREQ / PWM_PRESCALER / 50 / 1.03f / 2)
+#define OCR_60Hz          (uint16_t)(SYS_CLOCK_FREQ / PWM_PRESCALER / 60 / 2)
+#define OCR_50Hz          (uint16_t)(SYS_CLOCK_FREQ / PWM_PRESCALER / 50 / 2)
 
 void set_pwm_freq()
 {
@@ -39,13 +39,20 @@ void set_pwm_freq()
 
 int main(void)
 {
-	// setting core clock prescaker to 8 (default)
-	CLKPSR = 0b00000011;
+	// setting core clock prescaler to 8 (default)
+	CCP    = 0xD8;
+	CLKPSR = 0b0000010;
+
+	// selecting external clock
+	CCP    = 0xD8;
+	CLKMSR = 0b00000010;
+	
 	// setting up IO
-	DDRB  = 1 << PINB1; // TOD output
+	DDRB  = 1 << PINB2; // TOD output
 	PORTB = 0;
 
-	for(uint32_t i = 0; i < 100000; ++i){}
+	// waiting for voltages to settle (cautionary measure)
+	for(uint32_t i = 0; i < 1000; ++i){}
 
 	// PWM frequency is set at startup and never changes until power cycle
 	set_pwm_freq();
@@ -55,9 +62,21 @@ int main(void)
 	// - Clear Timer on compare match
 	// - Clock prescaler = 1
 	//
-	TCCR0A = 0b00010000;
+	TCCR0A = 0b00000000;
 	TCCR0B = 0b00011001;
 
-    while(1){}
+    while(1){
+		
+		// this little trick here is to fix the fact that I
+		// swapped PB0 and PB22 in hardware, anyway a few clock
+		// cycles at 1.5MHz are negligible compared to the
+		// generated 50-60Hz wave
+		
+		if(TIFR0 & 2)
+		{
+			PORTB = ~PORTB;
+			TIFR0 = 2;
+		}
+	}
 
 }
